6698248f9e3155ae3c8016b36ee9708b879fda70
Bernal <franjaberra@gmail.com>
2018-04-24 13:16:34 +0200

    Subo ejercicio2

---
+++ bernal-ramirez-francisco-javier/Ejercicio2.py
@@ -0,0 +1,10 @@
+age = 0
+name = ""
+print ("")
+age = int(input ("Hi, wha's your age?:\n"))
+if age < 18:
+	print ("You shall no pass")
+else:
+	name = input ("What's your name?:\n")
+	print ("Hi,", name)
+print("See ya")
